aa04983dd9b4a1ade5eda687a04db84e7cebf896
Your Name <you@example.com>
2020-06-17 18:19:17 +0800

    jerry

---
+++ Atom.Starter/Model/AtomProject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Atom.Starter.Model
+{
+    public class AtomProject
+    {
+        [Description("项目Id")]
+        public int Id { get; set; }
+
+        [Description("项目名称")]
+        public string Name { get; set; }
+
+        [Description("项目描述")]
+        public string Desc { get; set; }
+
+        [Description("项目图标")]
+        public string Icon { get; set; }
+
+        [Description("添加时间")]
+        public DateTime? AddTime { get; set; }
+
+        [Description("添加人")]
+        public int AddUserId { get; set; }
+
+        [Description("修改时间")]
+        public DateTime? EditTime { get; set; }
+
+        [Description("修改人")]
+        public int EditUserId { get; set; }
+
+        [Description("是否可用")]
+        public bool IsValid { get; set; }
+    }
+}
